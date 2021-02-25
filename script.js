6244a4f6be687bc8a0803ba9a5983b9f8c8e0822
Natalie Finnegan <nfinneg2@gmail.com>
2021-02-25 11:08:56 -0700

    function refactor

--- script.js
+++ script.js
@@ -26,135 +26,82 @@ btnsDiv.style.display = "block";
 };
 
 
-// //add content
-// function question1 () {
-// var answersArray1 = ["&lt javascript &gt", "&lt script &gt", "&lt style &gt", "&lt js &gt"];
-// var quest1 = questionsArray[0]    
-
-// //create elements
-// var pTag = document.createElement("p");
-// var ol = document.createElement("ol");
-// var li1 = document.createElement("li");
-// var li2 = document.createElement("li");
-// var li3 = document.createElement("li");
-// var li4 = document.createElement("li"); 
-// var button1 = document.createElement("button");
-// var button2 = document.createElement("button");
-// var button3 = document.createElement("button");
-// var button4 = document.createElement("button");
-
-// //add content
-// pTag.innerHTML = quest1;
-// button1.innerHTML = answersArray1[0];
-// button2.innerHTML = answersArray1[1];
-// button3.innerHTML = answersArray1[2];
-// button4.innerHTML = answersArray1[3];
-
-// //append to document
-// pTag.appendChild(ol);
-// questionsLoop.appendChild(pTag);
-// li1.appendChild(button1);
-// li2.appendChild(button2);
-// li3.appendChild(button3);
-// li4.appendChild(button4);
-// ol.appendChild(li1);
-// ol.appendChild(li2);
-// ol.appendChild(li3);
-// ol.appendChild(li4);
-
-
-// }
 
 var questionsOpts = 
 [
     {
         question: "Which HTML element do we use to reference our Javascript file?",
-        choices: ["&lt javascript &gt", "&lt script &gt", "&lt style &gt", "&lt js &gt"],
-        answer: "&lt script &gt",
+        choices: ["< javascript >", "< script >", "< style >", "< js >"],
+        answer: "< script >",
      },
     
     {
         question: "How do you call a function named 'myFunction'?",
         choices: ["call myFunction", "invoke myFunction()", "myFunction()", "console.log(myFunction())"],
-        answer: 3
+        answer: "myFunction()",
     },
 
     {
         question: "How do you add a comment in JavaScript?",
         choices: ["type 'add comment'","**//", "//", "!add comment!"],
-        answer: 3
+        answer: "//",
     },
 
     {
         question: "How do you declare a variable?",
         choices: ["var myVariable", "variable myVariable", "myVariable()", "v myVariable" ],
-        answer: 1
+        answer: "var myVariable",
     },
 
  ]
-
- var answersArray = ["var myVariable", "//", "myFunction()", "&lt script &gt"]
  
  
-//  button.forEach(function(btn){
-//     btn.addEventListener("click", function(){
-//       if (answersArray) {
-//           userNotify.innerText = "Correct!";
-//       }
-//       else {
-//           userNotify.innerText = "Wrong!";
-//       }
-//     });
-// })
 
 
 //diplaying first question
  function quizQuestions () {
     questionsLoop.innerText = questionsOpts[currentQuestionIndex].question;
     console.log(questionsOpts[currentQuestionIndex]);
+    let buttonArray = [];
     for (i= 0; i < questionsOpts[currentQuestionIndex].choices.length; i++) {
         var button = document.createElement("button");
         button.innerText = questionsOpts[currentQuestionIndex].choices[i];
         btnsDiv.appendChild(button);
+        buttonArray.push(button);
+        // console.log(button);
     };
-    button.forEach(function(btn){
-        btn.addEventListener("click", function(){
-          if (answersArray) {
-              userNotify.innerText = "Correct!";
-          }
-          else {
-              userNotify.innerText = "Wrong!";
-          }
-        });
-    })
-}
 
+     buttonArray.forEach(function(btn){
+        // console.log(btn);
+         btn.addEventListener("click", function(){
+             console.log(event.target);
+        //     if (button.innerText === questionsOpts[currentQuestionIndex].answer) {
+        //         userNotify.innerText = "Correct!";
+        //  }
+        //     else {
+        //         userNotify.innerText = "Wrong!";
+        //  }
+        // });
+     })
 
+ }
 
 
 function nextQuestion(){
     currentQuestionIndex++;
     questionsLoop.innerText = questionsOpts[currentQuestionIndex].question;
     console.log(questionsOpts[currentQuestionIndex]);
+    let buttonArray = [];
+
     for (i= 0; i < questionsOpts[currentQuestionIndex].choices.length; i++) {
-    var button = document.createElement("button");
-    button.innerText = questionsOpts[currentQuestionIndex].choices[i];
-    btnsDiv.appendChild(button);
+        var button = document.createElement("button");
+        button.innerText = questionsOpts[currentQuestionIndex].choices[i];
+        btnsDiv.appendChild(button);
+        buttonArray.push(button)
+    
     }
-    button.forEach(function(btn){
-        btn.addEventListener("click", function(){
-          if (answersArray) {
-              userNotify.innerText = "Correct!";
-          }
-          else {
-              userNotify.innerText = "Wrong!";
-          }
-        });
-    })
+    
 }
-
-// quizQuestions()
-
 
 
      
@@ -169,3 +116,5 @@ function nextQuestion(){
     
 //     var storedHighScores = localStorage.getItem("highScores")
 // }
+
+  //getQuestions()
